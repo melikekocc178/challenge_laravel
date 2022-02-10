8d0a2aeb72cc0712173efd564c62df6b3c6c22a1
melikekocc178 <melikekocc178@gmail.com>
2022-02-10 18:52:22 +0300

    Initial commit

---
+++ app/Models/Art.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Art extends Model
+{
+    use HasFactory;
+    protected $fillable=['artist_id', 'title'];
+
+    public function artist(){
+        return $this->belongsTo(Artist::class);
+    }
+}
