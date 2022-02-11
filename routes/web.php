64036eb56e366c502be3a1e29b017183d183dcda
melikekocc178 <melikekocc178@gmail.com>
2022-02-11 15:00:49 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\Admin\ArtistController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,13 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 
 Route::group(['prefix'=>'artists','middleware' => 'auth'], function () {
-    Route::get('/artist', [ArtistController::class, 'index'])->name('artists.index');
-    Route::get('/artist/{artist:id}', [ArtistController::class, 'show'])->name('artists.show');
+    Route::get('/', 'App\Http\Controllers\ArtistController@index')->name('artists.index');
+    Route::get('/artist/{artist:id}', 'App\Http\Controllers\ArtistController@show')->name('artists.show');
+});
+Route::group(['prefix'=>'admin','middleware' => ['auth', 'auth.admin']], function () {
+    Route::get('/artists/create', [ArtistController::class, 'create'])->name('admin.artists.create');
+    Route::post('/artists', [ArtistController::class, 'store'])->name('admin.artists.store');
+    Route::get('/artists/{artist:id}/edit', [ArtistController::class, 'edit'])->name('admin.artists.edit');
+    Route::patch('/artists/{artist:id}/update', [ArtistController::class, 'update'])->name('admin.artists.update');
+    Route::delete('/artists/{artist:id}/destroy', [ArtistController::class, 'destroy'])->name('admin.artists.destroy');
+
+
 });
 require __DIR__.'/auth.php';
